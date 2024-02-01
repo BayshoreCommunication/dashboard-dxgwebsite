4eea48fd21b4e7e598b49b9fa0f625c7b0a95c33
arsahak <arsahak99@gmail.com>
2024-02-01 18:37:34 +0600

    update

---
+++ config/index.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const CLOUDINARY_CLOUD_NAME = "djrkvmpxe";
+export const CLOUDINARY_UPLOAD_PRESET = "blog-dashboard";
+export const CLOUDINARY_FOLDER = "blog-dashboard";
