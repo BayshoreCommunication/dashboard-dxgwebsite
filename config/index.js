cf15b579127017403438fe84ea99ba3f698cda31
rahad-dev24 <rahad.dev24@gmail.com>
2025-02-11 11:23:32 +0600

    added digital ocean

--- config/index.js
+++ config/index.js
@@ -1,4 +1,15 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// Old Image Upload Configuration Cloudinary
+
 export const CLOUDINARY_CLOUD_NAME = "dmjgvdacq";
 export const CLOUDINARY_UPLOAD_PRESET = "dxg_dashboard";
 export const CLOUDINARY_FOLDER = "dxg_dashboard";
+
+// New Image Upload Configuration Digital Ocean
+
+export const DIGITALOCEAN_ENDPOINT = "https://nyc3.digitaloceanspaces.com";
+export const DIGITALOCEAN_API_KEY = "DO00HFNWVPYJBRTBVZZJ";
+export const DIGITALOCEAN_SECRET_ACCESS_KEY =
+  "VTaTd4LASdw+OjDylmiBOPa4GOD956R/25597C7ULX8";
+export const DIGITALOCEAN_FOLDER = "dxg-agency";
